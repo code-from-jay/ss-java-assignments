757a8c167b19c96c26348940e41f6da5702dd728
Jordan Ibarra <jnibarra02@protonmail.com>
2022-07-13 17:35:26 -0400

    Move lambdas out of dedicated methods

--- lesson5/Assignment2/Lambdas.java
+++ lesson5/Assignment2/Lambdas.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 import java.util.function.Function;
 
 public class Lambdas {
-    public static Function<Integer, Boolean> isOdd() {
-        return (i) -> (i % 2) != 0;
-    }
 
-    public static Function<Integer, Boolean> isPrime() {
-        return (i) -> {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+
+        Function<Integer, Boolean> isOdd = (i) -> (i % 2) != 0;
+        Function<Integer, Boolean> isPrime = (i) -> {
             if (i <= 1) return false;
             if (i <= 3) return true;
             if (i % 2 == 0 || i % 3 == 0) return false;
@@ -20,10 +20,7 @@ public class Lambdas {
 
             return true;
         };
-    }
-
-    public static Function<Integer, Boolean> isPalindrome() {
-        return (i) -> {
+        Function<Integer, Boolean> isPalindrome = (i) -> {
             int num = i;
             int rev = 0;
 
@@ -34,10 +31,7 @@ public class Lambdas {
 
             return i == rev;
         };
-    }
 
-    public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
 
         int tests = scanner.nextInt();
         for (int i = 0; i < tests; ++i) {
@@ -46,13 +40,13 @@ public class Lambdas {
 
             switch (operation) {
                 case 1:
-                    System.out.println(isOdd().apply(number) ? "ODD" : "EVEN");
+                    System.out.println(isOdd.apply(number) ? "ODD" : "EVEN");
                     break;
                 case 2:
-                    System.out.println(isPrime().apply(number) ? "PRIME" : "COMPOSITE");
+                    System.out.println(isPrime.apply(number) ? "PRIME" : "COMPOSITE");
                     break;
                 case 3:
-                    System.out.println(isPalindrome().apply(number) ? "PALINDROME" : "NOT PALINDROME");
+                    System.out.println(isPalindrome.apply(number) ? "PALINDROME" : "NOT PALINDROME");
                     break;
                 default:
                     System.out.println("INVALID OPERATION");
